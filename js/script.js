65629dca8237601d61c6b25a9a0ac7c3932e5dc4
Joyce Lee <87531763+joyceful-lee@users.noreply.github.com>
2025-03-03 21:08:35 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init({
+    duration: 1000,
+    offset: 100,
+});
+
+var modal = document.getElementById("myModal");
+var img = document.querySelectorAll(".sub img");
+var modalImg = document.getElementById("img01");
+
+img.forEach((imgA) => {
+    imgA.onclick = function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+});
+
+var span = document.getElementsByClassName("close-full")[0];
+span.onclick = function () {
+    modal.style.display = "none";
+}
